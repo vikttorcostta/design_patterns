3f3f8af5e64985d98f3909adeb15695f1c57b3df
Acer-Victor-Costa <victorfotografia12@gmail.com>
2025-08-30 13:13:27 -0300

    implementação de testes

--- test/strategy/services/PaymentGatewayServiceTest.spec.ts
+++ test/strategy/services/PaymentGatewayServiceTest.spec.ts
@@ -1,29 +1,53 @@
 import { PaymentGatewayService } from "../../../src/strategy/services/PaymentGateway.service";
 import { MercadoPago } from "../../../src/strategy/services/payment_gateways/MercadoPago";
-import { Assas } from "../../../src/strategy/services/payment_gateways/Asaas";
+import { Asaas } from "../../../src/strategy/services/payment_gateways/Asaas";
 import { Stripe } from "../../../src/strategy/services/payment_gateways/Stripe";
 
 let apiKey: any;
 let environment: any
 let timeout: any
 
-beforeEach(() => {
-    apiKey = 'api_key'
-    environment ='sandbox';
-    timeout = 500;
-})
+describe('casos de testes de PaymentGatewayService', () => {
 
-test('configura o gateway de pagamento mercado pago', () => {
+    // variaveis de configuração
+    const apiKey = 'api_key';
+    const environment = 'sandbox';
+    const timeout = 500;
+
+    // instanciação das strategy
     const mercadoPago = new MercadoPago(apiKey, environment, timeout);
-    expect(mercadoPago).toBeTruthy()
-});
-
-test('configura o gateway de pagamento asaas', () => {
-    const asaas = new Assas(apiKey, environment, timeout);
-    expect(asaas).toBeTruthy();
-});
-
-test('configura o gateway de pagamento stripe', () => {
     const stripe = new Stripe(apiKey, environment, timeout);
-    expect(stripe).toBeTruthy();
+    const asaas = new Asaas(apiKey, environment, timeout);
+
+    beforeEach(() => {
+        mercadoPago;
+        stripe;
+        asaas;
+    })
+
+    test('configura o gateway de pagamento mercado pago', () => {
+        const mercadoPago = new MercadoPago(apiKey, environment, timeout);
+        expect(mercadoPago).toBeTruthy()
+    });
+
+    test('configura o gateway de pagamento asaas', () => {
+        const asaas = new Asaas(apiKey, environment, timeout);
+        expect(asaas).toBeTruthy();
+    });
+
+    test('configura o gateway de pagamento stripe', () => {
+        const stripe = new Stripe(apiKey, environment, timeout);
+        expect(stripe).toBeTruthy();
+    });
+
+    test('faz um pagamento com sucesso -  mercado pago', () => {
+        const strategyMercadoPago = new PaymentGatewayService(mercadoPago);
+        expect(strategyMercadoPago.pay(500, 'BRL')).toBeTruthy();
+    })
+
+    test('faz um pagamento falhar - mercado pago', () => {
+        const strategyStripe = new PaymentGatewayService(stripe);
+        expect(strategyStripe.pay('sasa', 'sasa')).toBeFalsy();
+    })
+
 });
